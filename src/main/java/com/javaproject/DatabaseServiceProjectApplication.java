34eede5923d2c0312c87a93ce9c2b318d160cfcb
ClickOps-Technologies <101456989+ClickOps-Technologies@users.noreply.github.com>
2024-05-10 00:01:11 +0530

    Update DatabaseServiceProjectApplication.java

--- src/main/java/com/javaproject/DatabaseServiceProjectApplication.java
+++ src/main/java/com/javaproject/DatabaseServiceProjectApplication.java
@@ -2,7 +2,7 @@ package com.javaproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-#---
+
 @SpringBootApplication
 public class DatabaseServiceProjectApplication {
 
